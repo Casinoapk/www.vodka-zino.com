c40d003ccbd71ccd68ca9556280670b9d11cbb80
Casinoapk <stelelena7@gmail.com>
2025-11-10 19:08:24 +0300

    Update data-update.js

--- data-update.js
+++ data-update.js
@@ -1,11 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
     function updateMeta() {
         const now = new Date();
-        const meta = document.getElementById("https://vodka-casino.ru/");
+        const meta = document.getElementById("https://vod-ka.bet/");
         if (meta) {
             meta.setAttribute("content", now.toISOString());
         }
     }
     updateMeta();
     setInterval(updateMeta, 100);
+
 });
